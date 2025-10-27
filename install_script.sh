f276880c046061713921b38cde0f487bf1bfd00e
Дмитрий Пушкин <d.pushkin@limecreditgroup.com>
2025-10-27 18:01:01 +0300

    фикс скрипт

--- install_script.sh
+++ install_script.sh
@@ -1,6 +1,6 @@
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount --yes-wipe-all-disks ./hosts/beliar-laptop/disko.nix
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz home-manager
-nix-channel --update
-sudo useradd -m beliar
-sudo passwd beliar
+sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount --yes-wipe-all-disks ./hosts/beliar-laptop/disko.nix && \
+nix-channel --add https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz home-manager && \
+nix-channel --update && \
+sudo useradd -m beliar && \
+sudo passwd beliar && \
 sudo nixos-install --flake ./#beliar-laptop
