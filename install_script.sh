78a481037bf35082254fb05dc425f7a2d9d27ea4
BeliaR1232 <beliar1232@gmail.com>
2025-10-28 08:28:12 +0300

    поправил скрипт установки

--- install_script.sh
+++ install_script.sh
@@ -1,6 +1,6 @@
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount --yes-wipe-all-disks ./hosts/beliar-laptop/disko.nix && \
+nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount --yes-wipe-all-disks ./hosts/beliar-laptop/disko.nix && \
 nix-channel --add https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz home-manager && \
 nix-channel --update && \
-sudo useradd -m beliar && \
-sudo passwd beliar && \
-sudo nixos-install --flake ./#beliar-laptop
+nixos-install --flake ./#beliar-laptop && \
+echo "Установите пароль для beliar" && \
+nixos-enter --root /mnt -c 'passwd beliar'
